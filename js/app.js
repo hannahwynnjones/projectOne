e246ea1d55eb4970fc0085226ca8cac0d5d02dc0
Hannah Wynn Jones <hannahwynnjones@hotmail.co.uk>
2017-02-10 13:01:06 +0000

    second commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,51 @@
-console.log('GET YOUR OWN BACK!!!')
+console.log('GET YOUR OWN BACK!!!');
+
+//Introduction page loads
+    //logo swipes past
+
+//Instructions page loads
+    //logo
+    //Heading = 'Instructions'
+    //Paragraph = instructions
+    //Player 1 clicks - 'Go button!''
+
+//Ready page loads
+    //Player x presses the 'Player 'x' Ready?'
+
+//Question page loads
+    //Randomly select question to ask from Array. (1)
+    //Timer starts (10 seconds to answer) (2)
+    //Question on top
+    //3 options ->player chooses from 3 options.
+    //player presses "submit button"
+    //run function answer (3)
+
+//run functionWin (4)
+
+// Ready page loads
+    //Player x presses the 'Player 'x' Ready?'
+
+//Loop round to next player starting.
+  //always run functionWin after each go.
+
+//
+
+
+//functions:
+
+// (1) function randomQuestion  (generating a random question from the pre-set array of questions)
+// displays question on page
+
+// (2) function for timer starting, and stops when it hits zero.  When timer hits zero, and no button has been hit, player looses point (treat like a wrong answer).
+
+// (3) function answer:
+// if player answer === true answer) {
+// return 'Correct!'
+// move div one up, one left
+// scoreOne++
+// else (no change)
+// run new page, next player
+
+// (4) functionWin (checking to see if anyone has hit 5 points yet)
+// if (scoreOne || scoreTwo === 5)
+//
