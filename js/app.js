1821c6f6c1ce961e0a35670dd81de1975f04ac8c
Hannah Wynn Jones <hannahwynnjones@hotmail.co.uk>
2017-02-13 15:40:51 +0000

    ccs and player 1 always wins

--- js/app.js
+++ js/app.js
@@ -7,13 +7,18 @@ $(() => {
   // let time = 10;
   let questionCounter = 0; //Tracks question number to decide whose go it is
   let popped;
+  let playerOne;
+  let playerTwo;
   let currentPlayer = 'playerOne';
 
   // const $timer = $('.timer');
-  const $button = $('.start');
-  const $reset = $('.reset');
+  // const $button = $('.start');
+  // const $reset = $('.reset');
   const $questionDiv = $('.question');
   const $daveDiv = $('.daveDiv');
+  // const $playerOne = $('.playerOne');
+  // const $playerTwo = $('.playerTwo');
+  // const $result =$('.result');
 
   var listOfQuestions = [
     {
@@ -22,14 +27,44 @@ $(() => {
       correctAnswer: 'Bring it all back'
     },
     {
-      question: 'What is 2+5',
+      question: 'What is 2+5?',
       choices: ['3', '7', '11'],
       correctAnswer: '7'
     },
     {
-      question: 'What is my favourite colour',
-      choices: ['3', 'blue', 'yellow'],
-      correctAnswer: 'blue'
+      question: 'What is my favourite colou?r',
+      choices: ['3', 'black', 'yellow'],
+      correctAnswer: 'yellow'
+    },
+    {
+      question: 'What colour is the ball?',
+      choices: ['cat', '7', 'red'],
+      correctAnswer: 'red'
+    },
+    {
+      question: 'What is my favourite food?',
+      choices: ['3', 'chocolate', 'yellow'],
+      correctAnswer: 'chocolate'
+    },
+    {
+      question: 'What colour is the car?',
+      choices: ['cat', 'red', 'blue'],
+      correctAnswer: 'red'
+    },
+    {
+      question: 'What is my favourite colour?',
+      choices: ['purple', 'cat', 'car'],
+      correctAnswer: 'purple'
+    },
+    {
+      question: 'What colour is the orange?',
+      choices: ['orange', '7', 'fish'],
+      correctAnswer: 'orange'
+    },
+    {
+      question: 'Who is the queen?',
+      choices: ['Liz', 'cake', 'donald duck'],
+      correctAnswer: 'Liz'
     }
   ];
 
@@ -80,7 +115,6 @@ $(() => {
   const $answers = $('.answers');
   $answers.on('click', 'button', checkForMatch);
 
-  generateQuestion();  //always generate a question at the end of each go
 
 //(3) Check for a match
   function checkForMatch(e) {
@@ -88,15 +122,19 @@ $(() => {
     const userAnswer = $(e.target).text();
 
     if (correctAnswer === userAnswer || currentPlayer === playerOne) {
-      console.log('Correct');
+      console.log('Correctp1');
       $daveDiv.text('Correct');
       scoreOne++;
+      console.log('scoreOne');
+      console.log(scoreOne);
       // Dave - Thats correct!  noise
       //chair moves one space up
     } else if (correctAnswer === userAnswer || currentPlayer === playerTwo) {
-      console.log('Correct');
+      console.log('Correctp2');
       $daveDiv.text('Correct');
       scoreTwo++;
+      console.log('scoreTwo');
+      console.log(scoreTwo);
       // Dave - Thats correct!  noise
       //chair moves one space up
     } else {             //(correctAnswer !== userAnswer)
@@ -104,49 +142,59 @@ $(() => {
       $daveDiv.text('Incorrect');
       // Dave - Thats Incorrect!  noise
     }
+    generateQuestion();   //always generate a question at the end of each go
     questionCounter++;
+    console.log('scoreOne');
+    console.log(scoreOne);
+    console.log('scoreTwo');
+    console.log(scoreTwo);
+    console.log('questionCounter');
+    console.log(questionCounter);
+    togglePlayer();                 //change players
+    console.log(currentPlayer);
+    playerOneWin();          //check for player one win
+    playerTwoWin();
   }
 
-  playerOneWin();
-  playerTwoWin();
-  generateQuestion();
-  togglePlayer();
+  playerOneWin();          //check for player one win
+  playerTwoWin();          //check for player one win
+  generateQuestion();      //always generate a question at the end of each go
 
-  console.log(scoreOne);
-  console.log(scoreTwo);
+  // questionCounter++;
 
-// (4) functionWin (checking to see if anyone has hit 5 points yet)
-// if (scoreOne || scoreTwo === 5)
+  // console.log(scoreOne);
+  // console.log(scoreTwo);
+  // console.log(questionCounter);
+
 //
-//(4) chnage between player one and player two.
+//(4) change between player one and player two.
   function togglePlayer() {
     currentPlayer = currentPlayer === 'playerOne' ? 'playerTwo' : 'playerOne';
   }
 
 // (5) function to determine whether player one has won yet:
   function playerOneWin() {
-    if (scoreOne >= 5) {
+    if (scoreOne === 5) {
       $daveDiv.text('Player One has won!');
+      console.log('p1 wins');
       //play dunk music
     }
   }
   function playerTwoWin() {
-    if (scoreOne >= 5) {
-      $daveDiv.text('Player One has won!');
+    if (scoreTwo === 5) {
+      $daveDiv.text('Player Two has won!');
+      console.log('p2 wins');
       //play dunk music
     }
   }
+//(6) reset game at end
+
+// $('button.reset').on('click', ()=>{
+//   //location.reload();
+//   $playerOne.text('');
+//   $playerTwo.text('');
+//   $result.text('');
+//   $scoreOne.text(`${scoreOne}`);
+//   $scoreTwo.text(`${scoreTwo}`);
+  console.log(currentPlayer);
 });
-
-
-//
-
-
-// (5) function to determine whether player one has won yet:
-
-// if player answer === true answer) {
-// return 'Correct!'
-// move div one up, one left
-// scoreOne++
-// else (no change)
-// run new page, next player
