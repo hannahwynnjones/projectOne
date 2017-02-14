dd6a5696d1d8ed182557dcdf2f0b44d84f8c36dd
Hannah Wynn Jones <hannahwynnjones@hotmail.co.uk>
2017-02-14 09:19:09 +0000

    qus added timer doesnt stop

--- js/app.js
+++ js/app.js
@@ -7,17 +7,20 @@ $(() => {
   // let time = 10;
   let questionCounter = 0; //Tracks question number to decide whose go it is
   let popped;
-  let playerOne;
-  let playerTwo;
+  // let playerOne;
+  // let playerTwo;
   let currentPlayer = 'playerOne';
+  let time = 10;
 
   // const $timer = $('.timer');
-  const $button = $('.start');
-  // const $reset = $('.reset');
+
+  const $reset = $('.reset');
   const $questionDiv = $('.question');
   const $daveDiv = $('.daveDiv');
-  const $start = $('.start');
+  // const $start = $('.start');
   const $timer = $('.timer');
+  const $startButton = $('.start');
+  const $hidden = $('.hidden');
 
   // const $result =$('.result');
   // const $playerOne = $('.playerOne');
@@ -32,73 +35,73 @@ $(() => {
       correctAnswer: 'Bring it all back'
     },
     {
-      question: 'What is 2+5?',
-      choices: ['3', '7', '11'],
-      correctAnswer: '7'
+      question: 'Which of these was a character in The Dandy and not The Beano?',
+      choices: ['Minnie the Minx', 'Ivy the Terrible', 'Beryl the Peril'],
+      correctAnswer: 'Minnie the Minx'
     },
     {
-      question: 'What is my favourite colou?r',
-      choices: ['3', 'black', 'yellow'],
-      correctAnswer: 'yellow'
+      question: 'Tamagotchi" is a Japanese portmanteau of what two words?',
+      choices: ['"Egg and "watch"', '"Egg and "computer"', '"Egg and "monster"'],
+      correctAnswer: '"Egg and "watch"'
     },
     {
-      question: 'What colour is the ball?',
-      choices: ['cat', '7', 'red'],
-      correctAnswer: 'red'
+      question: 'What was the name of the sheep who was the first successfully cloned animal in 1996?',
+      choices: ['William', 'Molly', 'Dolly'],
+      correctAnswer: 'Dolly'
     },
     {
-      question: 'What is my favourite food?',
-      choices: ['3', 'chocolate', 'yellow'],
-      correctAnswer: 'chocolate'
+      question: 'Who is the only Blue Peter presenter ever to have been sacked?',
+      choices: ['Katy Hill', 'Konnie Huq', 'Richard Bacon'],
+      correctAnswer: 'Richard Bacon'
     },
     {
-      question: 'What colour is the car?',
+      question: 'What name was shared by all four members of the popular girl gang in Recess?',
+      choices: ['Courtney', 'Ashley', 'Stacey'],
+      correctAnswer: 'Ashley'
+    },
+    {
+      question: 'What colour were Chuckie\'s glasses in Rugrats?',
+      choices: ['purple', 'red', 'green'],
+      correctAnswer: 'purple'
+    },
+    {
+      question: 'Which of these was NOT a character on Playdays?',
+      choices: ['Handy Andy', 'The Whey Bird', 'Peggy Patch'],
+      correctAnswer: 'Handy Andy'
+    },
+    {
+      question: 'What type of coin was The Queen\'s Nose?',
+      choices: ['10p', '£2', '50p'],
+      correctAnswer: '50p'
+    },
+    {
+      question: 'Which Teenage Mutant Ninja Turtle wore a red bandana?',
+      choices: ['Michelangelo', 'Donatello', 'Raphael'],
+      correctAnswer: 'Raphael'
+    },
+    {
+      question: 'What don\'t the Backstreet Boys care about?',
+      choices: ['Who you are, where you\'re from and what you did', 'What you did, what you look like and where you come from', 'who you are, what your name is and what\'s your favourite dish'],
+      correctAnswer: 'Who you are, where you\'re from and what you did'
+    },
+    {
+      question: 'What was Ms. Frizzle\'s first name in "The Magic School Bus" series?',
+      choices: ['Veronica', 'Vanessa', 'Valerie'],
+      correctAnswer: 'Valerie'
+    },
+    {
+      question: 'Which Pokemon was not in the original Pokedex?',
       choices: ['cat', 'red', 'blue'],
       correctAnswer: 'red'
     },
     {
-      question: 'What is my favourite colour?',
-      choices: ['purple', 'cat', 'car'],
+      question: 'What was the name of Angelica Pickles\' doll?',
+      choices: ['purple', 'Cynthia', 'Verity'],
       correctAnswer: 'purple'
     },
     {
-      question: 'What colour is the orange?',
-      choices: ['orange', '7', 'fish'],
-      correctAnswer: 'orange'
-    },
-    {
-      question: 'What is 2+5?',
-      choices: ['3', '7', '11'],
-      correctAnswer: '7'
-    },
-    {
-      question: 'What is my favourite colou?r',
-      choices: ['3', 'black', 'yellow'],
-      correctAnswer: 'yellow'
-    },
-    {
-      question: 'What colour is the ball?',
-      choices: ['cat', '7', 'red'],
-      correctAnswer: 'red'
-    },
-    {
-      question: 'What is my favourite food?',
-      choices: ['3', 'chocolate', 'yellow'],
-      correctAnswer: 'chocolate'
-    },
-    {
-      question: 'What colour is the car?',
-      choices: ['cat', 'red', 'blue'],
-      correctAnswer: 'red'
-    },
-    {
-      question: 'What is my favourite colour?',
-      choices: ['purple', 'cat', 'car'],
-      correctAnswer: 'purple'
-    },
-    {
-      question: 'What colour is the orange?',
-      choices: ['orange', '7', 'fish'],
+      question: 'The Princess in the Super Mario franchise hasn\'t always been named Peach (in North America, at least). What was she previously known as?',
+      choices: ['Princess Toadstool', '7', ''],
       correctAnswer: 'orange'
     },
     {
@@ -115,52 +118,54 @@ $(() => {
 
 // function for timer starting, and stops when it hits zero.  When timer hits zero, and no button has been hit, player looses point (treat like a wrong answer).
 
-$button.on('click', startTimer);
-$submit.on('click', checkForMatch);
+  $startButton.on('click', startTimer);
+  $startButton.on('click', generateQuestion);
 
-function startTimer() {
-  resetGame();
-  toggleBoard();
-  generateSum();
-  $timer.addClass('active');
-
-  const timerId = setInterval(() => {
-    time--;
-    $timer.html(time);
-  }, 1000);
-
+//
+  function startTimer() {
+    console.log(startTimer);
+    $timer.addClass('active');
+    const timerId = setInterval(() => {
+      time--;
+      $timer.html(time);
+    }, 1000);
+  };
   setTimeout(() => {
     clearInterval(timerId);
     $display.html('Stop!');
     $button.html('Play again?');
     toggleBoard();
   }, 10000); // stop timer after 10 seconds
-}
 
-function generateSum() {
-  $input.val('');
-  const first = Math.ceil(Math.random() * 10);
-  const second = Math.ceil(Math.random() * 10);
-  $display.html(`${first} + ${second} = ?`);
-  computerAnswer = first + second;
-}
 
-function resetGame() {
-  userScore = 0;
-  time = 10;
-  $score.html(userScore);
-  $timer.html(time);
-  $feedback.html('');
-  $timer.removeClass('active');
-}
+//
+
+// function resetGame() {
+//   time = 10;
+//   $score.html(userScore);
+//   $timer.html(time);
+//   $feedback.html('');
+//   $timer.removeClass('active');
+// }
 
 function toggleBoard() {
   $hidden.toggle();
   $button.toggle();
 }
+//
+// $('button.reset').on('click', ()=>{
+//     //location.reload();
+//     $player1.text('');
+//     $player2.text('');
+//     $result.text('');
+//     $score1.text(`${scoreOne}`);
+//     $score2.text(`${scoreTwo}`);
+//       time = 10;
+//     }
 
 // (2) generate Question  (generating a qu from thearray by popping it out
 // displays question on page
+
 
   function generateQuestion() {
     // Pop last question from array
@@ -222,7 +227,7 @@ function toggleBoard() {
 
   playerOneWin();          //check for player one win
   playerTwoWin();          //check for player one win
-  generateQuestion();      //always generate a question at the end of each go
+  // generateQuestion();      //always generate a question at the end of each go
 
 //showng the scoreboard at the bottom of the page to keep tabs on the score without having to use the console.
   $score1.text(`${scoreOne}`);
@@ -248,14 +253,23 @@ function toggleBoard() {
       //play dunk music
     }
   }
+
+//finsih game?
+//big dave speaks and finsihes the game, player then given the option of re-starting
+
 //(6) reset game at end
 
-// $('button.reset').on('click', ()=>{
+  $reset.on('click', ()=>{
 //   //location.reload();
-//   $playerOne.text('');
-//   $playerTwo.text('');
-//   $result.text('');
-//   $scoreOne.text(`${scoreOne}`);
-//   $scoreTwo.text(`${scoreTwo}`);
-  console.log(currentPlayer);
+    // $playerOne.text(0);
+    // $playerTwo.text(0);
+    //   $result.text('');
+    $score1.text(0);
+    $score2.text(0);
+    console.log(currentPlayer);
+    console.log(scoreOne);
+    console.log(scoreTwo);
+    console.log(questionCounter);
+    time = 10;
+  });
 });
